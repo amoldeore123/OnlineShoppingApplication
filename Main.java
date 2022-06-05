ff69880220b88630c97dfef1a903b10ea0168479
amoldeore123 <106860461+amoldeore123@users.noreply.github.com>
2022-06-05 10:28:19 +0530

    main file added

---
+++ Main.java
@@ -0,0 +1,27 @@
+
+public class Main 
+{
+	public static void main(String[] args) 
+	{
+		int i, j;
+		int sum = 0;
+		int count = 0;
+		int [] arr = {1, 4, 6, 2, 7, 9};
+		int x = 11;
+		
+		for(i=0; i<arr.length-1; i++)
+		{
+			for(j=i+1; j<arr.length; j++)
+			{
+				sum = arr[i] + arr[j];
+				if(sum == x)
+				{
+					count++;
+				}
+			}
+		}
+		System.out.println("Number of pairs : " + count);
+		
+	}
+
+}
